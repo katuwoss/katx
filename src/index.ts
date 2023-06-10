5583423c23b9a785e86ef601c4a5f59a43dee6fa
Jakub Kropáček <kropikuba@gmail.com>
2023-06-10 10:50:27 +0200

    Added slug generation

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-import express from "express";
 import type { Request } from "express";
+
+import express from "express";
 import { PrismaClient } from "@prisma/client";
+import { nanoid } from "nanoid";
 import morgan from "morgan";
 import helmet from "helmet";
 import path from "path";
+import { URL } from "url";
 
 const app = express();
 const prisma = new PrismaClient();
@@ -13,6 +16,8 @@ const port = process.env.PORT || 80;
 app.use(helmet());
 app.use(morgan("short"));
 app.use("/", express.static(path.join(__dirname, "../public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.post(
   "/",
@@ -20,9 +25,32 @@ app.post(
     req: Request<unknown, unknown, { slug?: string; url: string }>,
     res
   ) => {
-    /*
-        Generate new entry
-    */
+    let slug = req.body.slug || nanoid(16);
+    let valid;
+    let i = 0;
+
+    do {
+      valid = await prisma.url.findFirst({
+        where: {
+          slug: slug,
+        },
+      });
+      if (valid === null) {
+        break;
+      }
+      slug = nanoid(16 + i);
+    } while (i++ <= 50);
+    if (i === 50) {
+      res.redirect("/error=Failed to create slug");
+      return;
+    }
+    await prisma.url.create({
+      data: {
+        slug: slug,
+        url: req.body.url,
+      },
+    });
+    res.redirect("/");
   }
 );
 
